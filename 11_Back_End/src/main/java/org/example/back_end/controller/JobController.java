aa16a877ebee28050d074297cfde54d2e22df5f1
Udara San <udarassanjeewa@gmail.com>
2025-07-15 10:35:14 +0530

    backend

---
+++ 11_Back_End/src/main/java/org/example/back_end/controller/JobController.java
@@ -0,0 +1,24 @@
+package org.example.back_end.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.back_end.dto.JobDTO;
+import org.example.back_end.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api/v1/job")
+@RestController
+@RequiredArgsConstructor
+public class JobController {
+
+    //constructor injection
+    private final JobService jobService;
+
+    @PostMapping("create")
+    public void createJob(@RequestBody JobDTO jobDTO){
+        jobService.saveJob(jobDTO);
+    }
+}
---
+++ 11_Back_End/src/main/java/org/example/back_end/service/JobService.java
@@ -0,0 +1,13 @@
+package org.example.back_end.service;
+
+import org.example.back_end.dto.JobDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JobService {
+
+    //logic to save job
+    public void saveJob(JobDTO jobDTO) {
+        //db operation
+    }
+}
