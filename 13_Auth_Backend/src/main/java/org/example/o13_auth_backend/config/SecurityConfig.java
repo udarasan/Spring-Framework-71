0dbd13326839a5f54ed369d85f9fd0fbc7b597bc
Udara San <udarassanjeewa@gmail.com>
2025-07-31 14:49:28 +0530

    fix filter chain issue

--- 13_Auth_Backend/src/main/java/org/example/o13_auth_backend/config/SecurityConfig.java
+++ 13_Auth_Backend/src/main/java/org/example/o13_auth_backend/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
@@ -35,7 +36,8 @@ public class SecurityConfig {
                                        SessionCreationPolicy.STATELESS)
                )
                .authenticationProvider(authenticationProvider())
-               .addFilter(jwtAuthFilter);
+               .addFilterBefore(jwtAuthFilter
+                       , UsernamePasswordAuthenticationFilter.class);
        return http.build();
     }
 
