2aa038f62fbc2eb4f97a3894705acec2e0e16008
Udara San <udarassanjeewa@gmail.com>
2025-07-31 14:45:20 +0530

    handle mappings

---
+++ 13_Auth_Backend/src/main/java/org/example/o13_auth_backend/controller/AuthController.java
@@ -0,0 +1,37 @@
+package org.example.o13_auth_backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.o13_auth_backend.dto.ApiResponse;
+import org.example.o13_auth_backend.dto.AuthDTO;
+import org.example.o13_auth_backend.dto.RegisterDTO;
+import org.example.o13_auth_backend.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<ApiResponse> registerUser(
+            @RequestBody RegisterDTO registerDTO){
+        return ResponseEntity.ok(
+               new ApiResponse(
+                       200,
+                       "User registered successfully",
+                       authService.register(registerDTO)
+               )
+        );
+    }
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse> login(@RequestBody AuthDTO authDTO){
+        return ResponseEntity.ok(new ApiResponse(200,
+                "OK",authService.authenticate(authDTO)));
+    }
+
+}
