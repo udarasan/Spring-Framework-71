962566d954d70b86cbc809abd75645694f9e5bae
Udara San <udarassanjeewa@gmail.com>
2025-07-31 14:21:20 +0530

    auth service

---
+++ 13_Auth_Backend/src/main/java/org/example/o13_auth_backend/service/AuthService.java
@@ -0,0 +1,34 @@
+package org.example.o13_auth_backend.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.o13_auth_backend.dto.AuthDTO;
+import org.example.o13_auth_backend.dto.AuthResponseDTO;
+import org.example.o13_auth_backend.entity.User;
+import org.example.o13_auth_backend.repository.UserRepository;
+import org.example.o13_auth_backend.util.JwtUtil;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    public AuthResponseDTO authenticate(AuthDTO authDTO){
+        // validate credentials
+        User user=userRepository.findByUsername(authDTO.getUsername())
+                .orElseThrow(()->new RuntimeException("User not found"));
+        // check password
+        if (!passwordEncoder.matches(
+                authDTO.getPassword(),
+                user.getPassword())){
+            throw new BadCredentialsException("Invalid credentials");
+        }
+        // generate token
+        String token=jwtUtil.generateToken(authDTO.username);
+        return new AuthResponseDTO(token);
+    }
+}
